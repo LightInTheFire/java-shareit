8f698afb1aac3fb2b41b8e8d6452ea9dbd284131
Ilia Egorov <aleilmessia@mail.ru>
2025-10-25 18:39:02 +0300

    feat: added duplicate data exception

--- src/main/java/ru/practicum/shareit/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/practicum/shareit/exception/GlobalExceptionHandler.java
@@ -4,20 +4,20 @@ import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 class GlobalExceptionHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleException(Exception ex) {
+    public ErrorResponse onException(Exception ex) {
         log.error("Error occurred while processing request {}", ex.getMessage());
         return new ErrorResponse("internal server error",
                 "An error occurred while processing request");
@@ -57,5 +57,12 @@ class GlobalExceptionHandler {
     public ErrorResponse onNotFoundException(NotFoundException ex) {
         log.warn("Not found exception occurred while processing request {}", ex.getMessage());
         return new ErrorResponse("not found", ex.getMessage());
+    }
+
+    @ExceptionHandler(DuplicateDataException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse onDuplicateDataException(DuplicateDataException ex) {
+        log.warn("Duplicate data exception occurred while processing request {}", ex.getMessage());
+        return new ErrorResponse("duplicate data", ex.getMessage());
     }
 }
