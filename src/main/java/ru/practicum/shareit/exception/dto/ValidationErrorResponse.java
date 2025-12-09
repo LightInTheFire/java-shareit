6b6f77a2a15ef646e3ebf68d46785267c93ca89c
Ilia Egorov <aleilmessia@mail.ru>
2025-12-09 23:02:46 +0300

    fix: changed name from violations to error because of tests

--- src/main/java/ru/practicum/shareit/exception/dto/ValidationErrorResponse.java
+++ src/main/java/ru/practicum/shareit/exception/dto/ValidationErrorResponse.java
@@ -2,5 +2,5 @@ package ru.practicum.shareit.exception.dto;
 
 import java.util.List;
 
-public record ValidationErrorResponse(List<Violation> violations) {
+public record ValidationErrorResponse(List<Violation> error) {  //violations are better name but test expect error
 }
